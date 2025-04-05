a3505c2713bc1347de08dd0108b12a2174c9c70b
Nuttakit Kundum <nuttakitkundum@gmail.com>
2025-04-05 19:13:03 +0700

    update HashKey contract

--- contracts/src/HashKeyKYC.sol
+++ contracts/src/HashKeyKYC.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract HashKeyKYC is Ownable {
     mapping(address => bool) private kycStatus;
+
+    constructor() Ownable(msg.sender) {}
 
     function checkKYC(address _user) external view returns (bool) {
         return kycStatus[_user];
